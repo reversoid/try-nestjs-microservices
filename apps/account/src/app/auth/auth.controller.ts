8ee9c48d79ab8e4aead92982c84579e1e7062918
George <ancient.gekob@gmail.com>
2022-10-16 15:47:30 +0300

    implemented validation

--- apps/account/src/app/auth/auth.controller.ts
+++ apps/account/src/app/auth/auth.controller.ts
@@ -1,17 +1,19 @@
 import { Body, Controller } from '@nestjs/common';
 import { AccountLogin, AccountRegister } from '@school/contracts';
 import { AuthService } from './auth.service';
-import {RMQRoute} from 'nestjs-rmq';
+import {RMQRoute, RMQValidate} from 'nestjs-rmq';
 
 @Controller()
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @RMQValidate()
   @RMQRoute(AccountRegister.topic)
   async register(@Body() dto: AccountRegister.Request):Promise<AccountRegister.Response> {
     return this.authService.register(dto);
   }
 
+  @RMQValidate()
   @RMQRoute(AccountLogin.topic)
   async login(
     @Body() { email, password }: AccountLogin.Request
