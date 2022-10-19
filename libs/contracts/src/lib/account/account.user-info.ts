59049d2494abb8727e2a9959bb6a7ff2f9f73985
George <ancient.gekob@gmail.com>
2022-10-19 14:45:09 +0300

    created contract for query

---
+++ libs/contracts/src/lib/account/account.user-info.ts
@@ -0,0 +1,15 @@
+import { User } from '@school/interfaces';
+import {IsString} from 'class-validator';
+
+export namespace AccountUserInfo {
+  export const topic = 'account.user-info.query';
+
+  export class Request {
+    @IsString()
+    id: string;
+  }
+
+  export class Response {
+    user: Omit<User, 'passwordHash'>;
+  }
+}
